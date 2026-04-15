2dcabe42fe269e9c1d4a83280365164dcaefdb84
skeatw <skeatw@mail.ru>
2026-04-15 10:02:30 +0300

    login and registration done

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, request, redirect, make_response
 from datetime import datetime
 import secrets
-from db_manager import Manage
+from db_manager import manage
 
 app = Flask(__name__)
 
-def set_token():
+def set_token(username):
     token = secrets.token_hex(32)
+
+    manage.add_token_to_db(token=token, username=username)
     response = make_response(redirect('/'))
     response.set_cookie('user', token, max_age=60 * 60 * 24 * 5, httponly=True, secure=True)
+
     return response
 @app.route('/')
 def index():
@@ -39,45 +42,35 @@ def user():
 def registration():
     if request.method == 'POST':
 
-        response = set_token()
-
-        m = Manage()
-
         username = request.form.get('username')
         user_email = request.form.get('email')
         data = [username, user_email]
-        m.add_to_db_user(data)
+        manage.add_to_db_user(data)
 
+        response = set_token(username=username)
         return response
 
     return render_template('registration.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        m = Manage()
+    if request.method == 'GET':
+        get_cookie = request.cookies.get('user')
 
-        cookie = request.cookies.get('user')
-
-        username = request.form.get('username')
-        user_email = request.form.get('email')
-
-        if cookie is None:
-
-            response = set_token()
-
-
-            return response
-
+        if get_cookie is not None:
+            #TODO: получать по этим куки информацию о пользователе из бд и отдавать их на index.html
+            return redirect('/') # вернуть аккаунта пользователя
         else:
-            user_email_from_db = m.get_email(username=username)
-            if user_email_from_db == user_email:
-                return redirect('/')
-        #TODO: получить куки пользователя есть время куки не истекло, то сразу перейти в аккаунт, если истекло для пользователя с username должна совпадать отправленная им
+            return render_template('login.html')
 
-    return render_template('login.html')
+    username = request.form.get('username')
+    user_email = request.form.get('email')
+
+    if manage.is_there_a_user_in_db(username=username) and user_email == manage.get_email(username=username):
+        response = set_token(username)
+        return response
+    else:
+        return render_template('registration.html')
 
 if __name__ == '__main__':
-    manage = Manage()
-    manage.create_connection()
     app.run(debug=True)
--- db_manager.py
+++ db_manager.py
@@ -3,9 +3,12 @@ from datetime import datetime
 
 class Manage:
 
-    @classmethod
-    def create_connection(cls):
-        conn = sqlite3.connect('todo.db')
+    def __init__(self):
+        self.db_path = 'todo.db'
+        self._create_connection()
+
+    def _create_connection(self):
+        conn = sqlite3.connect(self.db_path)
         cur = conn.cursor()
 
         # TODO: сначала набросать как будет выглядеть бд, а потом уже создавать все таблицы
@@ -13,6 +16,7 @@ class Manage:
         CREATE TABLE IF NOT EXISTS user (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             username VARCHAR(50) NOT NULL UNIQUE,
+            token VARCHAR(32) UNIQUE,
             email VARCHAR(100) UNIQUE)''')
 
         cur.execute('''
@@ -30,41 +34,68 @@ class Manage:
         conn.commit()
         conn.close()
 
-    def __init__(self):
-        self.conn = sqlite3.connect('todo.db')
-
-    def __del__(self):
-        self.conn.close()
-
     def add_to_db_task(self, data: list[None | str | datetime | bool]) -> None:
         """
         Добавление в таблицу task информацию о созданной задачи
         :param data: список, в котором находится информация о созданной задачи
         :return: None
         """
-        cur = self.conn.cursor()
+        conn = sqlite3.connect(self.db_path)
+        cur = conn.cursor()
         title = data[0]
         desc = data[1]
         creation_time = data[2]
         status = data[3]
         cur.execute('INSERT OR IGNORE INTO tasks (title, description, creation_time, status) VALUES (?, ?, ?, ?)', (title, desc, creation_time, status))
-        self.conn.commit()
-        cur.close()
+        conn.commit()
+        conn.close()
 
     def add_to_db_user(self, data: list[str]) -> None:
-        cur = self.conn.cursor()
+        conn = sqlite3.connect(self.db_path)
+        cur = conn.cursor()
         username = data[0]
         email = data[1]
 
         cur.execute('INSERT OR IGNORE INTO user (username, email) VALUES (?, ?)', (username, email))
-        self.conn.commit()
-        cur.close()
+        conn.commit()
+        conn.close()
 
     def get_email(self, username: str) -> str:
-        cur = self.conn.cursor()
+        conn = sqlite3.connect(self.db_path)
+        cur = conn.cursor()
         user_email = cur.execute('SELECT email FROM user WHERE username = ?', (username, )).fetchone()[0]
 
+        conn.close()
         return user_email
 
-    def get_id_username(self, username):
-        pass
+    def get_id_by_token(self, token: str) -> int:
+        conn = sqlite3.connect(self.db_path)
+        cur = conn.cursor()
+
+        id_str = cur.execute('SELECT id FROM user WHERE token = ?', (token, )).fetchone()[0]
+        id_user = int(id_str)
+        conn.close()
+
+        return id_user
+
+    def add_token_to_db(self, token: str, username: str) -> None:
+        conn = sqlite3.connect(self.db_path)
+        cur = conn.cursor()
+
+
+        cur.execute('UPDATE user SET token = ? WHERE username = ? ', (token, username))
+
+        conn.commit()
+        conn.close()
+
+    def is_there_a_user_in_db(self, username: str) -> bool:
+        conn = sqlite3.connect(self.db_path)
+        cur = conn.cursor()
+
+        answer = cur.execute('SELECT id FROM user WHERE username = ?', (username, )).fetchone()
+        if answer is not None:
+            return True
+        return False
+
+
+manage = Manage()
